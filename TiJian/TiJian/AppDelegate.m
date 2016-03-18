00a4dab31eedacd41bfcdb7a7efed590f835b9fd
gaomeng <gaomengaz@163.com>
2016-03-18 16:27:24 +0800

    gm
    1 单品详情页看了又看图片一样的bug修复
    2 品牌店分类图片优化
    3 品牌店logo图改为方形
    4 品牌店点击不同分类显示相同结果bug修复

--- TiJian/TiJian/AppDelegate.m
+++ TiJian/TiJian/AppDelegate.m
@@ -20,7 +20,7 @@
 #import "WebviewController.h"//web
 #import "AppointDetailController.h"//预约
 #import "LogView.h"
-#import <Bugtags/Bugtags.h>//bugtags
+//#import <Bugtags/Bugtags.h>//bugtags
 
 #define kAlertViewTag_token 100 //融云token
 #define kAlertViewTag_otherClient 101 //其他设备登陆
