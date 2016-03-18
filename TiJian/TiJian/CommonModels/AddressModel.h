f5de007bec4af320d68c541e00fad1645e510a08
gaomeng <gaomengaz@163.com>
2016-03-18 14:47:49 +0800

    gm
    看了又看图片显示错误

--- TiJian/TiJian/CommonModels/AddressModel.h
+++ TiJian/TiJian/CommonModels/AddressModel.h
@@ -22,7 +22,7 @@
 @property(nonatomic,retain)NSString *zip_code;//邮政编码
 @property(nonatomic,retain)NSString *address;//详细地址如：北京市东城区东华门街道
 
-@property(nonatomic,retain)NSString *fee;//邮费
+@property(nonatomic,retain)NSString *fee; //邮费
 @property(nonatomic,retain)NSString *express_fee;//邮费
 
 @end
